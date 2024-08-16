16f1bde5016a4d0e250cde26280b46c28b6ff46c
MrWinRock <mrwinrock11@gmail.com>
2024-08-16 23:47:21 +0700

    update: bootstap implement

--- src/presentation/components/navbar/NavBar.jsx
+++ src/presentation/components/navbar/NavBar.jsx
@@ -1,29 +1,30 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import buildings from "./../../../data/building/buildings";
 import logo from "./../../assets/images/logo.png";
 
+import { Offcanvas, Button } from "react-bootstrap";
+import "./NavBar.css";
+
 const NavBar = ({ mapRef }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [show, setShow] = useState(false);
   const [selectedId, setSelectedId] = useState(null);
-  const [isVisible, setIsVisible] = useState(false);
-  const navbarRef = useRef(null);
+  const offcanvasRef = useRef(null);
 
   const toggleNavbar = () => {
-    setIsOpen(!isOpen);
+    setShow(!show);
   };
 
-  const openNavbar = () => {
-    setIsOpen(true);
+  const handleShowNavbar = () => {
+    setShow(true);
   };
 
-  const closeNavbar = useCallback(() => {
-    setIsOpen(false);
+  const handleCloseNavbar = useCallback(() => {
+    setShow(false);
   }, []);
 
   const handleAreaButtonClick = (id) => {
-    openNavbar();
+    handleShowNavbar();
     setSelectedId(id);
-    setIsVisible(true);
   };
 
   const handleButtonClick = (buildingNumber) => {
@@ -35,99 +36,99 @@ const NavBar = ({ mapRef }) => {
   };
 
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
-        closeNavbar();
+    const handleDocumentClick = (event) => {
+      if (
+        offcanvasRef.current &&
+        !offcanvasRef.current.contains(event.target) &&
+        !event.target.closest(".fixed-left") &&
+        !event.target.closest(".navbar-offcanvas")
+      ) {
+        handleCloseNavbar();
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [closeNavbar]);
-
-  const handleTransitionEnd = () => {
-    if (!isOpen || selectedId === null) {
-      setIsVisible(false);
+    if (show) {
+      document.addEventListener("mousedown", handleDocumentClick);
+    } else {
+      document.removeEventListener("mousedown", handleDocumentClick);
     }
-  };
 
-  const uniqueBuildingIds = new Set();
-  buildings.forEach((building) => {
-    uniqueBuildingIds.add(building.id);
-  });
+    return () => {
+      document.removeEventListener("mousedown", handleDocumentClick);
+    };
+  }, [show, handleCloseNavbar]);
 
   return (
-    <div
-      ref={navbarRef}
-      className={`navbar-wrapper fixed top-0 left-0 h-screen ${
-        isOpen ? "w-[13%]" : "w-[8%]"
-      } overflow-hidden z-[1000] transition-all duration-[600ms] ease`}
-    >
-      <div className="navbar-container h-full bg-[#333] text-white overflow-auto">
-        <div className=".navbar-header block relative p-5 border-b border-[#444]">
-          <div className="navbar-logo p-3">
-            <img src={logo} alt="Logo" className="w-[45px] py-[15px]" />
-          </div>
-          <button
-            className="navbar-toggle bg-transparent border-none cursor-pointer flex flex-col justify-center items-center"
+    <>
+      <div className="fixed-left d-flex align-items-center p-3">
+        <div className="header-container">
+          <img src={logo} alt="Logo" className="logo" />
+          <span className="devider"></span>
+          <Button
+            variant="link"
+            className="hamburger-btn"
             onClick={toggleNavbar}
           >
-            <span
-              className={`w-8 h-1 bg-white rounded-md mb-1.5 transition-transform duration-[300ms] ease ${
-                isOpen ? "translate-y-[10px] rotate-45" : ""
-              }`}
-            ></span>
-            <span
-              className={`w-8 h-1 bg-white rounded-md mb-1.5 transition-opacity duration-[300ms] ease ${
-                isOpen ? "opacity-0" : ""
-              }`}
-            ></span>
-            <span
-              className={`w-8 h-1 bg-white rounded-md transition-transform duration-[300ms] ease ${
-                isOpen ? "-translate-y-[10px] -rotate-45" : ""
-              }`}
-            ></span>
-          </button>
-        </div>
-
-        <div className="navbar-menu p-5">
-          {[...uniqueBuildingIds].map((id) => (
-            <div key={id} className="navbar-menu-wrapper mb-2">
-              <button
-                className="navbar-menu-item w-full bg-[#444] border-none text-white p-3 mb-2 text-center cursor-pointer rounded-md hover:bg-[#555] transition-all duration-[300ms] ease"
-                onClick={() => handleAreaButtonClick(id)}
-              >
-                เขตพื้นที่ {id}
-              </button>
-              {isVisible && selectedId === id && (
-                <div
-                  className={`navbar-menu-item-content ${
-                    isOpen && selectedId === id
-                      ? "relative visible opacity-100 max-h-screen"
-                      : "absolute invisible opacity-0 max-h-0"
-                  } bg-[#222] border-none text-white p-3 mx-2 rounded-lg transition-all duration-[500ms] ease-in-out overflow-hidden`}
-                  onTransitionEnd={handleTransitionEnd}
-                >
-                  {buildings
-                    .filter((building) => building.id === id)
-                    .map((building) => (
-                      <button
-                        key={building.number}
-                        className="navbar-menu-item-content-item w-full p-3 mb-2 text-center text-white bg-[#444] border-none rounded-md cursor-pointer hover:bg-[#555] transition-all duration-[300ms] ease"
-                        onClick={() => handleButtonClick(building.number)}
-                      >
-                        อาคาร {building.number}
-                      </button>
-                    ))}
-                </div>
-              )}
+            <div className={`hamburger ${show ? "open" : ""}`}>
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
-          ))}
+          </Button>
         </div>
       </div>
-    </div>
+
+      <div ref={offcanvasRef}>
+        <Offcanvas
+          show={show}
+          onHide={handleCloseNavbar}
+          placement="start"
+          scroll={true}
+          backdrop={false}
+          className="navbar-offcanvas p-3"
+        >
+          <Offcanvas.Header className="canvas-header">
+            <h1 className="display-5 font-weight-bold text-center z-index-100">
+              อาคาร
+            </h1>
+          </Offcanvas.Header>
+          <span className="devider"></span>
+          <Offcanvas.Body className="canvas-body">
+            {[...new Set(buildings.map((building) => building.id))].map(
+              (id) => (
+                <div key={id} className="w-100 mb-2">
+                  <Button
+                    variant="primary"
+                    className="area-button w-100 mb-2"
+                    style={{ background: "#222" }}
+                    onClick={() => handleAreaButtonClick(id)}
+                  >
+                    เขตพื้นที่ {id}
+                  </Button>
+                  {selectedId === id && (
+                    <div>
+                      {buildings
+                        .filter((building) => building.id === id)
+                        .map((building) => (
+                          <Button
+                            key={building.number}
+                            variant="secondary"
+                            className="building-button w-30 m-2"
+                            style={{ background: "#444" }}
+                            onClick={() => handleButtonClick(building.number)}
+                          >
+                            อาคาร <strong>{building.number}</strong>
+                          </Button>
+                        ))}
+                    </div>
+                  )}
+                </div>
+              )
+            )}
+          </Offcanvas.Body>
+        </Offcanvas>
+      </div>
+    </>
   );
 };
 
