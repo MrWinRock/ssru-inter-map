2c9f90af876c3f396f05abfde7b40727f02770cd
MrWinRock <mrwinrock11@gmail.com>
2025-08-06 13:19:27 +0700

    fix: deploy config please

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '.',
+    base: '/ssru-inter-map/',
     server: {
         port: 3000,
         open: true
     },
     build: {
         outDir: 'dist',
-        sourcemap: true
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'],
+                    bootstrap: ['react-bootstrap', 'bootstrap'],
+                    i18n: ['react-i18next', 'i18next', 'i18next-browser-languagedetector']
+                }
+            }
+        }
+    },
+    optimizeDeps: {
+        include: ['react', 'react-dom', 'react-bootstrap', 'react-i18next']
     }
 })
